59e729e44fac34c5c23422cf9864b3ad065715a1
Romain Boutelet <rboutelet@gmail.com>
2024-11-30 19:05:25 -0500

    advanced a lil bit

--- stf_test.R
+++ stf_test.R
@@ -19,12 +19,6 @@ dat$id <- paste0(sprintf("%02d", dat$statecd),
                  sprintf("%03d", dat$countycd),
                  sprintf("%05d", dat$plot))
 
-dat_count <- dat %>%
-  group_by(id) %>%
-  summarise("n_count" = n())
-
-dat <- left_join(dat, dat_count, by = id)
-
 shp <- stunitco[stunitco$COUNTYFIPS %in% dat$fips,]
 
 sf_dat <- st_as_sf(dat, coords = c("lon_public", "lat_public"), crs = 5070)
@@ -42,14 +36,98 @@ sf_data_wgs84 <- st_transform(sf_dat, crs = 4326)
 sf_data_wgs84$colors <- "blue"
 sf_data_wgs84$colors[sf_data_wgs84$drybio_ag_live_5plus == 0] <- "red"
 
-leaflet(subset(sf_data_wgs84, id == "16308186024")) %>%
+leaflet(subset(sf_data_wgs84, id != 0)) %>%
   # Add an Esri basemap (e.g., Esri.WorldImagery)
   addProviderTiles(providers$Esri.WorldImagery) %>%
   # Add your sf object to the map (default style for sf objects)
   addCircleMarkers(
-    radius = ~(drybio_ag_live_5plus)/5,
+    radius = ~(drybio_ag_live_5plus)/2,
     color = ~colors
     )
 
-subset(dat, n_count > 2)
+new_count <- dat %>% 
+  group_by(id) %>% 
+  summarise("n" = n())
+
+newlist <- list()
+count <- 1
+for (i in subset(new_count, id %in% new_count[new_count$n > 2,]$id)$id){
+  mydat <- subset(dat, id == i)
+  for (j in 1:(nrow(mydat)-1)){
+    newlist[[count]] <- data.frame(id = i, measyear = mydat$measyear[j],
+                                   diff = mydat$drybio_ag_live_5plus[j+1]-
+                                     mydat$drybio_ag_live_5plus[j],
+                                   range = max(mydat$drybio_ag_live_5plus)-
+                                     min(mydat$drybio_ag_live_5plus))
+    count <- count + 1
+  }
+}
+
+worst_dat <- do.call(rbind, newlist)
+
+worst <- which.min(worst_dat$diff)
+
+
+
+my_id <- subset(worst_dat, id %in% new_count[new_count$n > 2,]$id)[worst,]$id
+
+
+for (i in subset(dat, id %in% new_count[new_count$n > 2,]$id)$id){
+  x <- subset(dat, id == i)$measyear
+  y <- subset(dat, id == i)$drybio_ag_live_5plus
+  plot(x[order(x)], y[order(x)],
+       type = "l", ylim = range(0, subset(dat, id == i)$drybio_ag_live_5plus))
+  readline(prompt = "Pause. Press <Enter> to continue...")
+}
+
+
+sf_blackhills <- st_crop(sf_data_wgs84, xmin = -106, xmax = -90, ymin = 0,
+                         ymax = 90)
+
+leaflet(subset(sf_blackhills, id != 0)) %>%
+  # Add an Esri basemap (e.g., Esri.WorldImagery)
+  addProviderTiles(providers$Esri.WorldImagery) %>%
+  # Add your sf object to the map (default style for sf objects)
+  addCircleMarkers(
+    radius = ~(drybio_ag_live_5plus)/2,
+    color = ~colors
+  )
+
+
+new_count_bh <- sf_blackhills %>% 
+  group_by(id) %>% 
+  summarise("n" = n())
+
+newlist_bh <- list()
+count <- 1
+for (i in subset(sf_blackhills, id %in% new_count_bh[new_count_bh$n > 2,]$id)$id){
+  mydat <- subset(sf_blackhills, id == i)
+  for (j in nrow(mydat)-1){
+    newlist_bh[[count]] <- data.frame(id = i, measyear = mydat$measyear[j],
+                                   diff = mydat$drybio_ag_live_5plus[j+1]-
+                                     mydat$drybio_ag_live_5plus[j],
+                                   range = max(mydat$drybio_ag_live_5plus)-
+                                     min(mydat$drybio_ag_live_5plus))
+    count <- count + 1
+  }
+}
+
+worst_sf_bh <- do.call(rbind, newlist_bh)
+
+worst <- which.max(worst_dat$range)
+
+
+
+my_id <- subset(dat, id %in% new_count[new_count$n > 2,]$id)[worst,]$id
+
+
+for (i in subset(dat, id %in% new_count[new_count$n > 2,]$id)$id){
+  x <- subset(dat, id == i)$measyear
+  y <- subset(dat, id == i)$drybio_ag_live_5plus
+  plot(x[order(x)], y[order(x)],
+       type = "l", ylim = range(0, subset(dat, id == i)$drybio_ag_live_5plus))
+  readline(prompt = "Pause. Press <Enter> to continue...")
+}
+
+
 
